b045bbe1989f898c645c002270456d3b335b5c4e
guyzsarun <sarun.nunt@gmail.com>
2022-05-13 14:44:11 +0700

    Update app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
+import os
+
 
 app = FastAPI()
 
@@ -10,10 +12,21 @@ def hello_world():
 
 
 @app.get("/ping")
-def hello_world():
-    return {"pong"}
+def ping():
+    return "pong"
+
+
+@app.get("/status")
+def status():
+    env = os.environ
+    return {
+        "HOSTNAME": env.get("HOSTNAME"),
+        "K_REVISION": env.get("K_REVISION"),
+        "PORT": env.get("PORT"),
+        "K_CONFIGURATION": env.get("K_CONFIGURATION"),
+    }
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
 
+    uvicorn.run(app, host="0.0.0.0", port=8080)
